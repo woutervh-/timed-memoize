83bf9bdb31c42dd0313f8a7b7cf44f4984ad3225
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-10-10 09:20:51 +0200

    Removed ununsed  object

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function simple(args) {
     return args;
 }
 
-function memoized(fn, cache, last, cleanup, options) {
+function memoized(fn, cache, cleanup, options) {
     const {timeout = 0, hot = true, discardUndefined = false, resolver = simple} = options;
 
     return function () {
@@ -11,7 +11,6 @@ function memoized(fn, cache, last, cleanup, options) {
 
         function cleanupCallback() {
             delete cache[key];
-            delete last[key];
             delete cleanup[key];
         }
 
@@ -19,7 +18,6 @@ function memoized(fn, cache, last, cleanup, options) {
             const returnValue = fn.apply(null, arguments);
             if (!discardUndefined || typeof returnValue !== 'undefined') {
                 cache[key] = returnValue;
-                last[key] = now;
                 cleanup[key] = setTimeout(cleanupCallback, timeout);
             }
         } else if (hot) {
@@ -37,15 +35,13 @@ export default function timedMemoize(a, b) {
         const fn = a;
         const options = b || {};
         const cache = {};
-        const last = {};
         const cleanup = {};
-        return memoized(fn, cache, last, cleanup, options);
+        return memoized(fn, cache, cleanup, options);
     } else if (typeof a === 'object' || arguments.length === 0) {
         const options = a || {};
         const cache = {};
-        const last = {};
         const cleanup = {};
-        return memoized((x, y) => y, cache, last, cleanup, {...options, resolver: args => args[0], discardUndefined: true});
+        return memoized((x, y) => y, cache, cleanup, {...options, resolver: args => args[0], discardUndefined: true});
     } else {
         throw new Error('Invalid arguments');
     }
