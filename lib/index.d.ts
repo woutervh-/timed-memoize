56e576964e671f2d92c729c8af407fc38b4d0b1a
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-11-02 12:01:03 +0100

    Added TypeScript types

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+export interface Options {
+    timeout?: number;
+    hot?: boolean;
+    discardUndefined?: boolean;
+    resolver?: (args: any[]) => string;
+}
+export default function timedMemoize<T>(a?: ((...args: any[]) => T) | Options, b?: Options): () => T;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-function simple(args) {
-    return args;
+export interface Options {
+    timeout?: number;
+    hot?: boolean;
+    discardUndefined?: boolean;
+    resolver?: (args: any[]) => string;
 }
 
-function memoized(fn, cache, cleanup, options) {
+function simple(args: any[]): string {
+    return args.toString();
+}
+
+function memoized<T>(fn: (...args: any[]) => T, cache: {[key: string]: T}, cleanup: {[key: string]: number}, options: Options): () => T {
     const {timeout = 0, hot = true, discardUndefined = false, resolver = simple} = options;
 
     return function () {
         const now = Date.now();
-        const key = resolver([...arguments]);
+        const key = resolver(Array.from(arguments));
 
         function cleanupCallback() {
             delete cache[key];
@@ -15,7 +22,7 @@ function memoized(fn, cache, cleanup, options) {
         }
 
         if (!(key in cache)) {
-            const returnValue = fn.apply(null, arguments);
+            const returnValue = fn.apply(null, arguments) as T;
             if (!discardUndefined || typeof returnValue !== 'undefined') {
                 cache[key] = returnValue;
                 cleanup[key] = setTimeout(cleanupCallback, timeout);
@@ -29,19 +36,20 @@ function memoized(fn, cache, cleanup, options) {
     };
 }
 
-export default function timedMemoize(a, b) {
+export default function timedMemoize<T>(a?: ((...args: any[]) => T) | Options, b?: Options): () => T {
     if (typeof a === 'function') {
         // Memoized function value
         const fn = a;
         const options = b || {};
-        const cache = {};
-        const cleanup = {};
+        const cache: {[key: string]: T} = {};
+        const cleanup: {[key: string]: number} = {};
         return memoized(fn, cache, cleanup, options);
     } else if (typeof a === 'object' || arguments.length === 0) {
+        // Memoized key-value pairs
         const options = a || {};
-        const cache = {};
-        const cleanup = {};
-        return memoized((x, y) => y, cache, cleanup, {...options, resolver: args => args[0], discardUndefined: true});
+        const cache: {[key: string]: T} = {};
+        const cleanup: {[key: string]: number} = {};
+        return memoized((x: any, y: T) => y, cache, cleanup, {...options, resolver: (args) => args[0].toString(), discardUndefined: true});
     } else {
         throw new Error('Invalid arguments');
     }
