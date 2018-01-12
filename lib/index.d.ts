6d68f6abeb622398a486885e1588f8ff92f5ef59
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2018-01-12 14:34:55 +0100

    Build ES5 and ES6 with commonjs/es6 modules

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Options {
-    timeout?: number;
-    hot?: boolean;
-    discardUndefined?: boolean;
-    resolver?: (args: any[]) => string;
-}
-declare function timedMemoize<T, F extends (...args: any[]) => T>(func: F, options?: Options): F;
-declare function timedMemoize<T>(options?: Options): (key: string, value?: T) => T | undefined;
-export default timedMemoize;
