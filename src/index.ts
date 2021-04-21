116185da3ecd248d915f6976141f6b3b33e4da37
Wouter van Heeswijk <woutervh@gmail.com>
2021-04-21 16:44:28 +0200

    WIP: proactive option

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+type ReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+
 export interface Options {
     timeout?: number;
     hot?: boolean;
     discardUndefined?: boolean;
     one?: boolean;
+    proactive?: boolean;
     resolver?: (args: any[]) => string;
 }
 
@@ -14,10 +17,13 @@ function argsNotEquals(args1: any[], args2: any[]) {
     return args1.some((arg, index) => arg !== args2[index]);
 }
 
-function memoized<T, F extends (...args: any[]) => T>(fn: F, cache: { [Key: string]: T }, cleanup: { [Key: string]: number | undefined }, options: Options): F {
-    const { timeout = 0, hot = true, discardUndefined = false, resolver = simple, one = false } = options;
+function memoized<F extends (...args: any[]) => any>(fn: F, options: Options): F {
+    const { timeout = 0, hot = true, discardUndefined = false, resolver = simple, one = false, proactive = true } = options;
     const actualResolver = one ? () => 'key' : resolver;
+    const cache: { [Key: string]: ReturnType<F> | undefined } = {};
+    const cleanup: { [Key: string]: number | undefined } = {};
     let lastArgs: any[] | undefined = undefined;
+    // TODO: cleanup in case of non-proactive.
 
     return function (...args: any[]) {
         const key = actualResolver(args);
@@ -29,20 +35,28 @@ function memoized<T, F extends (...args: any[]) => T>(fn: F, cache: { [Key: stri
         }
 
         if (!(key in cache) || one && (lastArgs === undefined || argsNotEquals(args, lastArgs))) {
-            const returnValue = fn.apply(null, arguments) as T;
-            if (!discardUndefined || typeof returnValue !== 'undefined') {
+            const returnValue = fn.apply(null, args) as ReturnType<F>;
+            if (!discardUndefined || returnValue !== undefined) {
                 cache[key] = returnValue;
                 if (timeout >= 0) {
-                    cleanup[key] = setTimeout(cleanupCallback, timeout);
+                    if (proactive) {
+                        cleanup[key] = setTimeout(cleanupCallback, timeout);
+                    } else {
+                        cleanup[key] = Date.now() + timeout;
+                    }
                 }
             }
         } else if (hot) {
-            const oldCleanupCallback = cleanup[key];
-            if (oldCleanupCallback !== undefined) {
-                clearTimeout(oldCleanupCallback);
+            const oldCleanup = cleanup[key];
+            if (oldCleanup !== undefined && proactive) {
+                clearTimeout(oldCleanup);
             }
             if (timeout >= 0) {
-                cleanup[key] = setTimeout(cleanupCallback, timeout);
+                if (proactive) {
+                    cleanup[key] = setTimeout(cleanupCallback, timeout);
+                } else {
+                    cleanup[key] = Date.now() + timeout;
+                }
             }
         }
 
@@ -61,15 +75,11 @@ function timedMemoize<T, F extends (...args: any[]) => T>(a?: F | Options, b?: O
         // Memoized function value
         const fn = a;
         const options = b || {};
-        const cache: { [Key: string]: T } = {};
-        const cleanup: { [Key: string]: number | undefined } = {};
-        return memoized(fn, cache, cleanup, options);
+        return memoized(fn, options);
     } else if (typeof a === 'object' || arguments.length === 0) {
         // Memoized key-value pairs
         const options = a || {};
-        const cache: { [Key: string]: T | undefined } = {};
-        const cleanup: { [Key: string]: number | undefined } = {};
-        return memoized((x: string, y?: T) => y, cache, cleanup, { ...options, resolver: (args) => args[0].toString(), discardUndefined: true });
+        return memoized((x: string, y?: T) => y, { ...options, resolver: (args) => args[0].toString(), discardUndefined: true });
     } else {
         throw new Error('Invalid arguments');
     }
